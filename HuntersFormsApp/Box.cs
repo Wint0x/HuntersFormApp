b433936587e6b4762c38523f9d31681b768db7e9
Zamius <111395278+Zamius@users.noreply.github.com>
2022-12-08 12:12:15 +0200

    Update Box.cs

--- HuntersFormsApp/Box.cs
+++ HuntersFormsApp/Box.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -41,6 +41,24 @@ namespace HuntersFormsApp
             title = args[1];
 
             MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            return;
+        }
+
+        public static void WarningBox(params string[] args)
+        {
+
+            string message = null;
+            string title = null;
+
+            if (args.Count() != 2)
+            {
+                throw new ArgumentException("Argument count error, only 2 accepted");
+            }
+            message = args[0];
+            title = args[1];
+
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Warning);
 
             return;
         }
